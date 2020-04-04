17373bddc781da12c75338829a90b5f681ae8d30
asisbagga <asisbagga@gmail.com>
2020-04-05 01:55:06 +0530

    Inspection, Shipment Marks Setup Done

--- Backend/hstc/customer/forms.py
+++ Backend/hstc/customer/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Customer_Details, Customer
+from .models import Customer_Details, Customer, Inspection
+from django.utils.translation import gettext_lazy as _
 
 class customer_form(forms.ModelForm):
     class Meta:
@@ -28,10 +29,31 @@ class oneshipment_form(forms.Form):
 class partShipmentForm(forms.ModelForm):
     class Meta:
         model = Customer_Details
-        fields = ("shipment_number",'manufacturing_days',)
+        fields = ("shipment_number",
+                  'manufacturing_days',)
         widgets = {
             'manufacturing_days': forms.TextInput(attrs={'class':'form-control'}),        
             'shipment_number': forms.TextInput(attrs={'class':'form-control'}),        
             }
 
-
+class Inspection_Form(forms.ModelForm):
+    class Meta:
+        model = Inspection
+        fields = ("inspection_done_by",
+                  "inspection_remarks",
+                  "inspection_photo_1",
+                  "inspection_photo_2",
+                  "actual_inspection_date")
+        labels = {
+        'inspection_done_by': _('Inspector Name'),
+        'inspection_remarks': _('Inspector Remarks'),
+        'inspection_photo_1': _('Upload Inspection Image 1'),
+        'inspection_photo_2': _('Upload Inspection Image 2'),
+        'actual_inspection_date': _('Actual Visit Date'),        
+        }
+        widgets = {
+            'inspection_done_by': forms.TextInput(attrs={'class':'form-control'}),        
+            'inspection_remarks': forms.TextInput(attrs={'class':'form-control'}),                
+            'actual_inspection_date': forms.TextInput(attrs={'class':'form-control'}),        
+            }
+        
